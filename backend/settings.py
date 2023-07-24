c696f3de067a4dc4c104ab1ba2ea8560f4178d5e
a7med3365 <a7med3365@gmail.com>
2023-07-24 17:56:33 +0300

    allowing all hosts

--- backend/settings.py
+++ backend/settings.py
@@ -20,67 +20,67 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__y7-5-aq2%+(9*vdh4xg%y9av)qucvm%c@6_h2qf*bz5#gdy!'
+SECRET_KEY = "django-insecure-__y7-5-aq2%+(9*vdh4xg%y9av)qucvm%c@6_h2qf*bz5#gdy!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework_simplejwt',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'djoser',
-    'shop'
+    "rest_framework_simplejwt",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "djoser",
+    "shop",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = "backend.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project4',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project4",
         "USER": "postgres",
         "PASSWORD": "3365",
         "HOST": "127.0.0.1",
@@ -94,24 +94,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-AUTH_USER_MODEL ='shop.Custom_User'
+AUTH_USER_MODEL = "shop.Custom_User"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
@@ -119,9 +119,9 @@ REST_FRAMEWORK = {
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Bahrain'
+TIME_ZONE = "Asia/Bahrain"
 
 USE_I18N = True
 
@@ -131,24 +131,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Djoser Settings
 AUTHENTICATION_CLASSES = (
-    'djoser.authentication.BearerTokenAuthentication',  # This allows using Bearer token for API endpoints
+    "djoser.authentication.BearerTokenAuthentication",  # This allows using Bearer token for API endpoints
 )
 
 DJOSER = {
-    'SERIALIZERS': {
-        'user_create': 'shop.serializers.CustomUserCreateSerializer',
+    "SERIALIZERS": {
+        "user_create": "shop.serializers.CustomUserCreateSerializer",
     },
-    'PERMISSIONS': {
-        'user_create': ['rest_framework.permissions.AllowAny'],
+    "PERMISSIONS": {
+        "user_create": ["rest_framework.permissions.AllowAny"],
     },
 }
 
@@ -156,7 +156,11 @@ from datetime import timedelta
 
 # JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  # Customize the token expiration as needed
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),  # Customize the refresh token expiration as needed
-    'SLIDING_TOKEN_REFRESH_EXPIRATION': False,
+    "ACCESS_TOKEN_LIFETIME": timedelta(
+        minutes=60
+    ),  # Customize the token expiration as needed
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(
+        days=1
+    ),  # Customize the refresh token expiration as needed
+    "SLIDING_TOKEN_REFRESH_EXPIRATION": False,
 }
